0c3de0159ced9ff47cb1307cd3d1fe0145b00426
Graham Heath <heathg@google.com>
2019-02-25 22:08:55 -0600

    start with no sprites... trade off for npx

--- updateSprites.sh
+++ updateSprites.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 startingDir=$(pwd);
-echo '$' $startingDir;
 cd $INIT_CWD;
 mkdir sprites;
 cd sprites;
 curl -O https://alttpr.com/sprites;
 mv sprites sprites.json
-cp $startingDir/sprites/001.link.1.zspr $INIT_CWD/sprites/001.link.1.zspr
