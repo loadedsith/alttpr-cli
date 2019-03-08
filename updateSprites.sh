9d2dc9c81c922013b9e99df2ab7d185bce88df29
Graham Heath <heathg@google.com>
2019-03-08 12:21:29 -0600

    add some environment vars, ascii art, and clean up sprite downloading

--- updateSprites.sh
+++ updateSprites.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 startingDir=$(pwd);
+if [[ -z "${ALTTPR_SPRITES}" ]]; then
+  ALTTPR_SPRITES_PATH="$INIT_CWD/sprites"
+else
+  ALTTPR_SPRITES_PATH="${ALTTPR_SPRITES}"
+fi
+
 cd $INIT_CWD;
-mkdir sprites;
-cd sprites;
-curl -O https://alttpr.com/sprites;
-mv sprites sprites.json
+
+if [ ! -d "$ALTTPR_SPRITES_PATH" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  echo "Creating spites folder at $ALTTPR_SPRITES_PATH";
+  mkdir -p $ALTTPR_SPRITES_PATH;
+else
+  echo "Spites folder exists at $ALTTPR_SPRITES_PATH";
+fi
+
+cd $ALTTPR_SPRITES_PATH;
+
+echo "Downloading sprites information."
+
+curl --progress-bar https://alttpr.com/sprites > sprites.json
